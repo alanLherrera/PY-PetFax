fbe7b0841b75c7b19a51082b1cc0147242a53594
alanLherrera <108435763+alanLherrera@users.noreply.github.com>
2023-03-05 12:25:55 -0800

    working app

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,6 +1,24 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, request, redirect)
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/', methods=['GET','POST'])
+def index():
+  if request.method == 'POST':
+    submitter2 = request.form['submitter']
+    fact2 = request.form['fact']
+
+    new_fact = models.Fact(submitter=submitter2,fact=fact2)
+    models.db.session.add(new_fact)
+    models.db.session.commit()
+
+
+    return redirect('/facts')
+
+  results = models.Fact.query.all()
+  
+  return render_template('facts/index.html', facts=results)
 
 @bp.route('/new')
 def new():
